f61f25dce6f06c83e8e587e533d19ec5b8037e77
trungukleanthiu7i-jpg <trungu.kleanthi.u7i@student.ucv.ro>
2025-12-06 03:04:24 -0800

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL + "/api",
 });
 
 // Attach token to every request
