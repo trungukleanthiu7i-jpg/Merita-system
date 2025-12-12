738e89eb5f1bc21777c709189ab7f31b45257a74
Kleanthi Trungugit config --global user.email trungu.kleanthi.u7i@student.ucv.ro <trungu.kleanthi.u7i@student.ucv.ro>
2025-12-12 14:28:41 -0800

    Update ProductsPage, styles, and product images

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -57,6 +57,7 @@ export default function ProductsPage() {
               : `${API_URL}/images/placeholder.png`;
 
             return (
+<<<<<<< HEAD
               <div key={product._id} className="product-card">
 
                 {/* IMAGE */}
@@ -70,6 +71,22 @@ export default function ProductsPage() {
                     }}
                   />
                 </div>
+=======
+              <div className="product-card">
+                  <img
+                    src={imageSrc}
+                      style={{
+    width: "100%",
+    height: "250px",
+    objectFit: "contain",
+  }}
+                    alt={product.name || "Unnamed product"}
+                    onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = `${API_URL}/images/placeholder.png`;
+                    }}
+                />
+>>>>>>> 75cb481 (Update ProductsPage, styles, and  all product images)
 
                 <h3>{product.name || "Unnamed product"}</h3>
 
